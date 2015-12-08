a267d9e63fbb8da93107e55e3b6ff8de8a7b23c2
posty72 <posty72@gmail.com>
2015-12-08 23:50:50 +1300

    Homepage changes Rnd 1

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,8 @@
 (function() {
 
-  var imageList = document.getElementsByTagName('img');
 
+  // Load images after the fact
+  var imageList = document.getElementsByTagName('img');
   for (var i = 0, k = imageList.length; i < k; i++) {
     if (imageList[i].src.length <= 0) {
       imageList[i].src = imageList[i].getAttribute('data-src');
