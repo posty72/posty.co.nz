d39f00ac03caf79003dc4496e24db2452ed46a55
Josh Post <contact+bitbucket@posty.co.nz>
2021-09-06 10:39:43 +1200

    added homepage copy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,34 @@
 import * as React from "react";
-import { Feature } from "../components/feature";
 import { Layout } from "../components/layout";
 
 const IndexPage = () => {
     return (
         <Layout title="Josh Post - Software Engineer">
             <div className="constrain-width">
-                <Feature title="Hello" slug="/" excerpt="Hi there" />
+                <h1>{"Kia ora, I'm Josh"}</h1>
+                <p>
+                    I am a digital product creator that works with companies to
+                    build the best solutions for them and their customers.
+                </p>
+                <p>
+                    My career began with an internship at the start-up incubator
+                    Lightning Labs, I have worked with companies ranging from
+                    start-ups to corporations. I has experience in industries
+                    such as media, analytics, utilities, government,
+                    not-for-profits and hospitality.
+                </p>
+                <p>
+                    Although I am primarily a front-end engineer, I adapt to
+                    whatever solution is needed. This means I have experience
+                    with managing AWS resources, infrastructure-as-code with
+                    tools like Terraform and building APIs using Python or Go.
+                </p>
+                <p>
+                    I cover a broad spectrum of the digital product lifecycle,
+                    from ideation, research, and validation, right through to
+                    implementing Scrum methodoloy and delivering the end
+                    products.
+                </p>
             </div>
         </Layout>
     );
